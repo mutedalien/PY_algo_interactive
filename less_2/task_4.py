f178b814169024942b43f4f0417be45033626b21
mutedalien <chel_c@mail.ru>
2020-04-20 14:38:46 +0300

    Циклы, рекурсия, функции

---
+++ less_2/task_4.py
@@ -0,0 +1,10 @@
+def func(a, b):
+    if a == b:
+        return f'{a}'
+    if a > b:
+        return f'{a}, {func(a - 1, b)}'
+    if a < b:
+        return f'{a}, {func(a + 1, b)}'
+
+
+print(func(1000, 10))
